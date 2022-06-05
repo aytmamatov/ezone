2a794d16bf1ca4c530c9ec53cedf5d74327de02c
aytmamatov_musa <black.nobody.anb@gmail.com>
2022-06-05 16:18:44 +0600

    Added accordion.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const YA_MAP = 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A301e4097ea3bd5024008eb8fdf6d3d037757eb5020edc32571f05329b7e96279&amp;width=100%25&amp;height=600&amp;lang=ru_RU&amp;scroll=true'
+const faqPanel = document.querySelectorAll('.faq__panel');
+
 let ok = false;
 
 window.addEventListener('scroll', function () {
@@ -11,3 +13,11 @@ window.addEventListener('scroll', function () {
     }, 1000)
   }
 });
+
+faqPanel.forEach((currentPanel, idx) => {
+  currentPanel.addEventListener('click', () => {
+    faqPanel[idx - 1]?.classList.remove('faq__panel--active');
+    faqPanel[idx + 1]?.classList.toggle('faq__panel--bordered');
+    currentPanel.classList.toggle('faq__panel--active');
+  })
+})
