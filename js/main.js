e57e3992408a98bcf73cb033697b88f6ec29fa86
aytmamatov_musa <black.nobody.anb@gmail.com>
2022-06-26 16:54:51 +0600

    Rewrote the accordeon code.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const YA_MAP = 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A301e4097ea3bd5024008eb8fdf6d3d037757eb5020edc32571f05329b7e96279&amp;width=100%25&amp;height=600&amp;lang=ru_RU&amp;scroll=true'
-const faqPanel = document.querySelectorAll('.faq__panel');
+const faqAccordion = document.querySelector('.faq__accordion');
 
 let ok = false;
 
@@ -14,10 +14,9 @@ window.addEventListener('scroll', function () {
   }
 });
 
-faqPanel.forEach((currentPanel, idx) => {
-  currentPanel.addEventListener('click', () => {
-    faqPanel[idx - 1]?.classList.remove('faq__panel--active');
-    faqPanel[idx + 1]?.classList.toggle('faq__panel--bordered');
-    currentPanel.classList.toggle('faq__panel--active');
-  })
+faqAccordion.addEventListener('click', (event) => {
+  const currentPanel = event.target.closest('.faq__panel')
+  if (currentPanel) {
+    currentPanel.classList.toggle('faq__panel--active')
+  }
 })
